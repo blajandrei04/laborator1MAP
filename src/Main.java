c40c118a843b8df7d923114b80a23762f1845d29
blajandrei04 <andrei.florin.blaj@stud.ubbcluj.ro>
2024-10-12 14:46:37 +0300

    Aufgabe1

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,15 @@ public class Main {
     public static void main(String[] args) {
 
         int[] grades = {29,37,38,41,84,63};
+        int[] numbers = {4,8,17,13,16};
         Grades.nichtAusreichendeNoten(grades);
         Grades.calculateAverage(grades);
         Grades.abrundung(grades);
         Grades.maximalAbgerundeteteNote(grades);
+        Zahlen.maxi(numbers);
+        Zahlen.mini(numbers);
+        Zahlen.minSum(numbers);
+        Zahlen.maxSum(numbers);
+
     }
 }
---
+++ src/Zahlen.java
@@ -0,0 +1,45 @@
+public class Zahlen {
+    public static void maxi(int [] numbers){
+        int max = numbers[0];
+        for (int number : numbers) {
+            if (number > max)
+                max = number;
+        }
+        System.out.println("Maximal:" + max);
+    }
+    public static void mini(int [] numbers){
+        int min = numbers[0];
+        for (int number : numbers) {
+            if(number < min)
+                min = number;
+        }
+        System.out.println("Minimal:" + min);
+    }
+    public static void maxSum(int [] numbers){
+        int min = numbers[0];
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+            if (number < min) {
+                min = number;
+            }
+        }
+        int rez = sum-min;
+        System.out.println("Maximal sum:" + rez);
+
+    }
+    public static void minSum(int [] numbers){
+        int max = numbers[0];
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+            if (number > max) {
+                max = number;
+            }
+        }
+        int rez = sum-max;
+        System.out.println("Minimal sum:" + rez);
+    }
+}
